1fdfd1946311d8452ffbd865c575337e916233bd
duck <me@duck.me.uk>
2018-02-01 01:37:31 +0000

    Put portfolio images in the correct place (durr)
    Navbar links now link to the intended destination on by.duck.me.uk (hacky but it works)
    
    Signed-off-by: duck <me@duck.me.uk>

--- resources/views/partials/nav-data.blade.php
+++ resources/views/partials/nav-data.blade.php
@@ -1,5 +1,8 @@
+@php
+    $target_home_host = ($_SERVER['HTTP_HOST'] == 'by.duck.me.uk' ? 'duck.me.uk' : $_SERVER['HTTP_HOST'])
+@endphp
 <li class="nav-item">
-    <a class="nav-link" href="/">Home.</a>
+    <a class="nav-link" href="//{{$target_home_host}}">Home.</a>
 </li>
 <li class="nav-item {{$active_page == 'portfolio' ? 'active' : ''}}">
     <a class="nav-link" href="//by.duck.me.uk">Portfolio. @if($active_page == 'portfolio')<span class="sr-only">(current)</span>@endif</a>
@@ -8,5 +11,5 @@
     <a class="nav-link" href="//blog.duck.me.uk">Blog. @if($active_page == 'blog')<span class="sr-only">(current)</span>@endif</a>
 </li>
 <li class="nav-item {{$active_page == 'contact' ? 'active' : ''}}">
-    <a class="nav-link" href="//{{$_SERVER['HTTP_HOST']}}/contact">Contact. @if($active_page == 'contact')<span class="sr-only">(current)</span>@endif</a>
+    <a class="nav-link" href="//{{$target_home_host}}/contact">Contact. @if($active_page == 'contact')<span class="sr-only">(current)</span>@endif</a>
 </li>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -6,7 +6,7 @@
     </div>
 </div>
 <nav class="navbar navbar-expand-sm navbar-dark">
-    <a class="navbar-brand logo logo-navbar" href="/">duck.</a>
+    <a class="navbar-brand logo logo-navbar" href="//{{($_SERVER['HTTP_HOST'] == 'by.duck.me.uk' ? 'duck.me.uk' : $_SERVER['HTTP_HOST'])}}">duck.</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNaviMenu" aria-controls="navbarNaviMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
