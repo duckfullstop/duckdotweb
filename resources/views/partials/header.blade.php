e17aa4b9f035cb0ea6bc16b09a81f72b08f4a52d
duck <me@duck.me.uk>
2018-02-01 00:13:25 +0000

    Tweak header meta tags slightly
    Signed-off-by: duck <me@duck.me.uk>

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,15 +4,13 @@
 <meta name="description" content="duck. does stuff." />
 
 {{-- <meta name="source-repo" content="https://github.com/luaduck/duckdotweb" /> --}}
-
 <meta name="for-alice" content="https://www.youtube.com/watch?v=ZTnlnrwBRPE" />
 <meta name="for-sarah" content="https://www.youtube.com/watch?v=_hTSiU53iDY" />
-<meta name="for-helvetica" content="https://www.youtube.com/watch?v=ZAXJbW0Fyw0" />
 <meta name="for-me" content="https://www.youtube.com/watch?v=g4Qqu16ffVk" />
 <meta name="for-you" content="https://www.youtube.com/watch?v=AyR-AF9oJYQ" />
-
-<meta name="prologue" content="Explore the world. Make a difference to someone's life. Never, ever, let the tarnish of a few blacken who you truly are." />
-<meta name="prologue-2" content="And most importantly: have fun while you're doing it. - duck" />
+{{-- <meta name="for-helvetica" content="https://www.youtube.com/watch?v=ZAXJbW0Fyw0" /> --}}
+<meta name="mission" content="Explore the world. Tread new ground. Make a difference to someone else's life. Never let the tarnish of a few blacken who you truly are." />
+<meta name="mission-2" content="And most importantly: have fun while you're doing all of the above. - duck" />
 
 
 <link type="text/css" rel="stylesheet" href="/css/app.css" />
